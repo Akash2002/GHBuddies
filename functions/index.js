4a9cb9de30f2e2d485bf8e68d41b2c817854a648
Akash Veerappan <akashpl2002@outlook.com>
2019-07-02 20:31:59 -0400

    Fixation fucking hell

--- functions/index.js
+++ functions/index.js
@@ -81,11 +81,8 @@ exports.sendRequest = functions.https.onCall((data, context) => {
                             num2 = val.val();
                           });
 
-                          if (count < 2) {
-                            sendMessage(num1, "You have been matched with " + key + "! Please meet them in the lobby in the next 5 minutes.");
-                            sendMessage(num2, "You have been matched with " + fname + ' ' + lname + "! Please meet them in the lobby in the next 5 minutes.");
-                            count++
-                          }
+                          sendMessage(num1, "You have been matched with " + key + "! Please meet them in the lobby in the next 5 minutes.");
+                          sendMessage(num2, "You have been matched with " + fname + ' ' + lname + "! Please meet them in the lobby in the next 5 minutes.");
                           return;
                         }
                       });
